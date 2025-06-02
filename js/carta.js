acd9f48dd75c9e9a3a07be0f536b10dd63872a18
PQNelfox01.dev <nelfox0123@gmail.com>
2025-06-02 15:46:52 -0400

    <3
    Agrego archivos del proyecto

---
+++ js/carta.js
@@ -0,0 +1,2 @@
+// Si más adelante quieres animar corazones, sonido o efectos, lo agregamos aquí
+console.log("Carta cargada correctamente ❤️");
---
+++ js/script.js
@@ -0,0 +1,2 @@
+// Aquí puedes añadir más adelante efectos de corazones flotando o scroll animado
+document.getElementById('musicaFondo').volume = 0.3; // volumen suave
